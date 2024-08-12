b81f1d85e36fbfdfc8ff81d8789970fda1b16ae6
Thomas Gurr <tpgurr21@gmail.com>
2024-08-11 23:27:24 -0400

    parsing the JSON data for portugal

--- script.js
+++ script.js
@@ -6,5 +6,26 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://countries-api-836d.onrender.com/countries/portugal');
-data = request.send();
+request.open('GET', 'https://countries-api-836d.onrender.com/countries/name/portugal');
+request.send();
+
+
+request.addEventListener('load', function() {
+    console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    console.log(data.languages[0].name);
+
+    const html = `
+    <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`
+})
