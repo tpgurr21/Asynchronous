11e99c044cfd7999d8ef8863b7c2eebbae6bd49d
Thomas Gurr <tpgurr21@gmail.com>
2024-08-14 05:25:37 -0400

    built a promise and tested it

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ getCountryAndNeighbour('monaco');
 setTimeout(() => {
     console.log('1 second passed');
     setTimeout(() => {
-        console.log('2 second passed');
+        console.log('2 seconds passed');
             setTimeout(() => {
-            console.log('3 second passed');
+            console.log('3 seconds passed');
                 setTimeout(() => {
-                console.log('4 second passed');
+                console.log('4 seconds passed');
             }, 1000)
         }, 1000)
     }, 1000)
@@ -226,7 +226,7 @@ whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
 
-*/
+
 
 console.log('Test start');
 setTimeout(()=> console.log('0 sec timer'), 0);
@@ -239,3 +239,71 @@ Promise.resolve('Resolved promise 2').then(res=>{
 })
 
 console.log('Test end');
+
+
+
+const lotteryPromise = new Promise(function(resolve, reject){
+   
+    console.log('Lottery draw is happening 🔮')
+    setTimeout(function() {
+        if(Math.random() >= 0.5) {
+            resolve('You WIN 💰');
+        } else {
+            reject(new Error('You lost your money 💩'));
+    }
+    }, 2000)
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// const lottery = function(resolve, reject){
+//     if(Math.random() >= 0.5){
+//         console.log('You WIN 💰')
+//     } else {
+//         console.log('You lost your money 💩');
+//     }
+// }
+
+// lottery()
+
+// Promisifying setTimeout
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+wait(1)
+    .then(() => {
+        console.log('1 second passed');
+        return wait(1);
+    })
+    .then(() => {
+        console.log('2 second passed');
+        return wait(1);
+    })
+    .then(() => {
+        console.log('3 second passed');
+        return wait(1);
+    })
+    .then(() => console.log('4 seconds passed'));
+
+// setTimeout(() => {
+//     console.log('1 second passed');
+//     setTimeout(() => {
+//         console.log('2 seconds passed');
+//             setTimeout(() => {
+//             console.log('3 seconds passed');
+//                 setTimeout(() => {
+//                 console.log('4 seconds passed');
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.log(x));
+
+*/
+
+navigator.geolocation.getCurrentPosition()
