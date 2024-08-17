32be825ddcf15c6d88883465895368be372b1717
Thomas Gurr <tpgurr21@gmail.com>
2024-08-16 20:30:33 -0400

    running promises in parallel

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ const renderError = function(msg) {
     countriesContainer.insertAdjacentText('beforeend', msg);
     // countriesContainer.style.opacity = 1;
 }
+
+const getJSON = function(url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response => {
+
+        if(!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+        return response.json();
+    });
+};
 ///////////////////////////////////////
 
 
@@ -433,7 +442,7 @@ const lotteryPromise = new Promise(function(resolve, reject){
     }, 2000)
 });
 
-*/
+
 
 const getPosition = function() {
     return new Promise(function(resolve, reject) {
@@ -489,3 +498,24 @@ console.log('1: Will get location');
     }
     console.log('3: Finished getting location')
 })();
+
+*/
+
+const get3Countries = async function(c1, c2, c3) {
+    try {
+        // const [data1] = await getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`);
+        // const [data2] = await getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`);
+        // const [data3] = await getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`);
+        // console.log([data1.capital, data2.capital, data3.capital])
+
+        const data = await Promise.all([
+            getJSON(`https://countries-api-836d.onrender.com/countries/name/${c1}`), 
+            getJSON(`https://countries-api-836d.onrender.com/countries/name/${c2}`),
+            getJSON(`https://countries-api-836d.onrender.com/countries/name/${c3}`)
+        ])
+        console.log(data.map(d => d[0].capital));
+    } catch(err) {
+        console.log(err);
+    }
+};
+get3Countries('portugal', 'canada', 'tanzania');
