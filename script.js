05ef554da3dc1457cbf91e8b6a2ed56f6430facf
Thomas Gurr <tpgurr21@gmail.com>
2024-08-16 21:41:40 -0400

    challenge #3, finished the section

--- script.js
+++ script.js
@@ -519,7 +519,7 @@ const get3Countries = async function(c1, c2, c3) {
 };
 get3Countries('portugal', 'canada', 'tanzania');
 
-*/
+
 
 // Promise.race
 (async function() {
@@ -559,3 +559,103 @@ Promise.any([
     Promise.resolve('Success'),
     Promise.resolve('Another Success')
 ]).then(res => console.log(res));
+
+*/
+
+////////////CODING CHALLENGE #2 EXAMPLE////////////
+const imgContainer = document.querySelector('.images')
+
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+const createImage = function(imgPath) {
+    return new Promise(function(resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function() {
+            imgContainer.append(img);
+   //     imgContainer.insertAdjacentHTML('beforeend', html) 
+    resolve(img)
+    })
+    img.addEventListener('error', function() {
+        reject(new Error('Image not found'))
+})
+})
+
+}
+let currentImg;
+
+
+// createImage('img/img-1.jpg')
+//     .then(img => {
+//         currentImg = img;
+//         console.log('Image 1 loaded');
+//         return wait(2);
+//     })
+//     .then (() => {
+//         currentImg.style.display = 'none';
+//         return createImage('img/img-2.jpg')
+//     })
+//     .then ((img) => {
+//         currentImg = img;
+//         console.log('Image 2 loaded');
+//         return wait(2);
+//     })
+//     .then (() => {
+//         currentImg.style.display = 'none';
+//         return createImage('img/img-3.jpg')
+//     })
+//     .catch(err => {
+//     console.error(`${err.message}`)
+//     })
+
+    //////////////////CODING CHALLENGE #3////////////
+
+    // const wait = function(seconds) {
+    //     return new Promise(function(resolve) {
+    //         setTimeout(resolve, seconds * 1000);
+    //     });
+    // };
+
+/////// PART 1
+const loadNPause = async function() {
+    try{
+        // Load image 1
+        let img = await createImage('img/img-1.jpg');
+        console.log('Image 1 loaded');
+        await wait(2);
+        img.style.display = 'none';
+        // Load image 2
+        img = await createImage('img/img-2.jpg');
+        console.log('Image 2 loaded');
+        await wait(2);
+        img.style.display = 'none';
+        // Load image 3
+        img = await createImage('img/img-3.jpg');
+        console.log('Image 3 loaded');
+
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+// loadNPause();
+
+/////////////Part 2/////////////
+const loadAll = async function(imgArr) {
+    try{
+        const imgs = imgArr.map(async img => await 
+            createImage(img))
+        const imgsEl = await Promise.all(imgs)
+        console.log(imgsEl)
+        imgsEl.forEach(img => img.classList.add('parallel'));
+    } catch(err) {
+        console.error(err);
+    }
+}
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'])
